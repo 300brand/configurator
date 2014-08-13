a0f29e49c14b0fc8b52ca30ff772b1d6592f2f4a
jake <bigjake2s@gmail.com>
2014-08-13 14:11:23 -0400

    main: Filled in module registration and core types

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"flag"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"net/http"
+	"os"
 )
 
+type Handler interface {
+	Router(*mux.Router)
+}
+
+type Response struct {
+	Success  bool
+	Error    error
+	Response interface{}
+}
+
 var router = mux.NewRouter()
+var Listen = flag.String("listen", ":8080", "Listen addresss")
+
+func Register(name string, handler Handler) {
+	handler.Router(router.PathPrefix("/" + name + "/").Name(name).Subrouter())
+}
 
 func main() {
 	flag.Parse()
+	handler := handlers.CombinedLoggingHandler(os.Stderr, router)
+	http.ListenAndServe(*Listen, handler)
 }
